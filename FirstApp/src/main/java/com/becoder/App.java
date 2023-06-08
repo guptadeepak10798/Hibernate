bda34b6fd435b9bfe6ab49ebc7f4bb548c5b2bbf
Deepak Gupta <3124@nucsoft.local>
2023-06-08 11:46:35 +0530

    second commit

--- FirstApp/src/main/java/com/becoder/App.java
+++ FirstApp/src/main/java/com/becoder/App.java
@@ -7,23 +7,22 @@ import org.hibernate.cfg.Configuration;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    
-    {
-//        System.out.println( "Hello World!" );
-        
-        //XML based hibernate configuration 
-//        Configuration cfg = new Configuration();
-//        cfg.configure();   
-//        SessionFactory buildSessionFactory = cfg.buildSessionFactory();
-        
-        SessionFactory buildSessionFactory =new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
-        System.out.println(buildSessionFactory); //like db connection object 
-        
-        System.out.println(buildSessionFactory); 
-        
-    }
-    
+public class App {
+	public static void main(String[] args)
+
+	{
+		// System.out.println( "Hello World!" );
+
+		// XML based hibernate configuration
+		// Configuration cfg = new Configuration();
+		// cfg.configure();
+		// SessionFactory buildSessionFactory = cfg.buildSessionFactory();
+
+		SessionFactory buildSessionFactory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+		System.out.println(buildSessionFactory); // like db connection object
+
+		System.out.println(buildSessionFactory);
+
+	}
+
 }
