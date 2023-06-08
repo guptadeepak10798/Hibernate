059a2f04e5a25ef8116df11f50afa7da75797db3
Deepak Gupta <3124@nucsoft.local>
2023-06-08 15:18:41 +0530

    created new project using hibernate crud

---
+++ HibernateCRUD-APP/src/main/java/com/prog/HibernateCRUD_APP/App.java
@@ -0,0 +1,13 @@
+package com.prog.HibernateCRUD_APP;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
